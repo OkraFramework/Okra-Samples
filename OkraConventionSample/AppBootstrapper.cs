64ad359ca4a71d91a58e238675e5d4cdef33f82c
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2014-07-16 22:08:44 +0100

    Add OkraConventionSample project

---
+++ OkraConventionSample/AppBootstrapper.cs
@@ -0,0 +1,52 @@
+﻿using Okra;
+using Okra.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Composition.Convention;
+using System.Composition.Hosting;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OkraConventionSample
+{
+    public class AppBootstrapper : OkraBootstrapper
+    {
+        /// <summary>
+        /// Perform general initialization of application services.
+        /// </summary>
+        protected override void SetupServices()
+        {
+            // Configure the navigation manager to preseve application state in local storage
+            NavigationManager.NavigationStorageType = NavigationStorageType.Local;
+        }
+
+        protected override ContainerConfiguration GetContainerConfiguration()
+        {
+            // In this method we define the conventions for discovering types
+
+            ConventionBuilder conventionBuilder = new ConventionBuilder();
+
+            // Register any types ending in "Page" using the "OkraPage" contract with the corresponding "PageName"
+
+            conventionBuilder.ForTypesMatching(type => type.FullName.EndsWith("Page"))
+                             .Export(builder => builder.AsContractType<object>()
+                                                       .AsContractName("OkraPage")
+                                                       .AddMetadata("PageName", type => type.Name.Substring(0, type.Name.Length - 4)));
+
+            // Register any types ending in "ViewModel" using the "OkraViewModel" contract with the corresponding "PageName"
+
+            conventionBuilder.ForTypesMatching(type => type.FullName.EndsWith("ViewModel"))
+                             .Export(builder => builder.AsContractType<object>()
+                                                       .AsContractName("OkraViewModel")
+                                                       .AddMetadata("PageName", type => type.Name.Substring(0, type.Name.Length - 9)));
+
+            // Combine the newly created conventions with the default Okra App Framework container configuration
+
+            return GetOkraContainerConfiguration()
+                    .WithAssembly(typeof(AppBootstrapper).GetTypeInfo().Assembly, conventionBuilder);
+        }
+    }
+}
---
+++ OkraConventionSample/Pages/FooViewModel.cs
@@ -0,0 +1,42 @@
+﻿using OkraConventionSample.Common;
+using Okra.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace OkraConventionSample.Pages
+{
+    /// <summary>
+    /// A basic view model that provides characteristics common to most applications.
+    /// </summary>
+    public class FooViewModel : ViewModelBase
+    {
+        [ImportingConstructor]
+        public FooViewModel(INavigationContext navigationContext)
+            : base(navigationContext)
+        {
+        }
+
+        public string Title
+        {
+            get
+            {
+                return "This text is provided by FooViewModel.cs";
+            }
+        }
+    }
+}
